bf26674ba11fac905adda62ac54c76e88102b185
Nika Darab <nikadarab@pm.me>
2021-04-05 11:48:50 -0500

    landing page changes

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -21,13 +21,44 @@ export default class LandingPage extends Component {
                 <Link to="library ">Share</Link>
               </h1>
             </div>
+
             <br />
-            <div className="landing-lines">
-              <p>
-                Poetlandia is a free platform for creating and sharing
-                interesting collection of words.
-              </p>
+
+            <div className="rules-wrapper">
+              <div className="rules first">
+                <p>
+                  Poetlandia is a free platform for creating and sharing
+                  interesting collection of words.
+                </p>
+              </div>
+              <br />
+              <hr />
+              <div className="rules">
+                <p>
+                  <Link to="/drafts">Drafts</Link> is a private place for you to
+                  create, edit and delete your pieces.
+                </p>
+              </div>
+
+              <hr />
+              <div className="rules">
+                <p>
+                  <Link to="/collab">Collab</Link> is a public place accesible
+                  to users who are registered. All registered users can edit a
+                  collaboration piece. Only the original author of the piece can
+                  delete or publish it to the library.
+                </p>
+              </div>
+              <hr />
+              <div className="rules">
+                <p>
+                  <Link to="/library">Library</Link> is a public forum of all
+                  pieces that were published by their authors.Accessbile by all
+                  users who visit this app.
+                </p>
+              </div>
             </div>
+
             <br />
             {/* <div>
               <Link to="/register">Sign up</Link>
